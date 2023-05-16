add9da441ccd253b11038dc94797b159b69ef063
Kentaro <94794692+bap-ssbm@users.noreply.github.com>
2023-05-17 01:44:20 +0900

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="dark:bg-dark-blue bg-white flex justify-between px-[5%] py-8 shadow-md">
-        <RouterLink class="text-[23px] font-[800]" to="/">
+        <RouterLink class="md:text-[23px] font-[800]" to="/">
             Where in the world?
         </RouterLink>
         <DarkMode/>
